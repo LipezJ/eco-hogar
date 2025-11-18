4312f123f5e9c0ba9543f1bc79c4ca469acf49a9
LipezJ <juandavidlipez47@gmail.com>
2025-11-17 19:30:24 -0500

    set default db password and user

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from 'mysql2/promise';
 import * as schema from './schema.js';
 
-const connection = mysql.createPool(process.env.DATABASE_URL || "mysql://user:password@localhost:3306/database");
+const connection = mysql.createPool(process.env.DATABASE_URL || "mysql://un_usr:una_clave@localhost:3306/eco_hogar");
 
 export const db = drizzle(connection, { schema, mode: 'default' });
 
